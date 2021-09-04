483e1491e728bc890b40277188fc00fcd109ef82
vExcess <41875927+vExcess@users.noreply.github.com>
2021-09-04 12:28:41 -0700

    Update ka_pjs.js

--- ka_pjs.js
+++ ka_pjs.js
@@ -15,43 +15,43 @@ if (PJS_run_env.code) {
   document.body.appendChild(PJS_run_env.canvas);
 
   PJS_run_env.PJS_start = 
-  "var canvas = document.getElementById('PJS_canvas');" +
-  "var processing = new Processing(canvas, function(processing) {" +
-  "  processing.size(" + PJS_run_env.code.dataset.width + ", " + PJS_run_env.code.dataset.height + ");" +
-  "  processing.background(0xFFF);" +
-  "  var mouseIsPressed = false;" +
-  "  processing.mousePressed = function() {" +
-  "    mouseIsPressed = true;" +
-  "  };" +
-  "  processing.mouseReleased = function() {" +
-  "    mouseIsPressed = false;" +
-  "  };" +
-  "  var keyIsPressed = false;"+
-  "  processing.keyPressed = function() {" +
-  "    keyIsPressed = true;" +
-  "  };" +
-  "  processing.keyReleased = function() {" +
-  "    keyIsPressed = false;" +
-  "  };" +
-  "  function getSound(s) {" +
-  "    var url = 'sounds/' + s + '.mp3';" +
-  "    return new Audio(url);" +
-  "  }" +
-  "  function playSound(s) {" +
-  "    s.play();" +
-  "  }" +
-  "  function getImage(s) {" +
-  "    var url = 'https://www.kasandbox.org/programming-images/' + s + '.png';" +
-  "    processing.externals.sketch.imageCache.add(url);" +
-  "    return processing.loadImage(url);" +
-  "  }" +
-  "  processing.angleMode = 'degrees';" +
-  "  with(processing) {";
+  "var canvas = document.getElementById('PJS_canvas');\n" +
+  "var processing = new Processing(canvas, function(processing) {\n" +
+  "  processing.size(" + PJS_run_env.code.dataset.width + ", " + PJS_run_env.code.dataset.height + ");\n" +
+  "  processing.background(0xFFF);\n" +
+  "  var mouseIsPressed = false;\n" +
+  "  processing.mousePressed = function() {\n" +
+  "    mouseIsPressed = true;\n" +
+  "  };\n" +
+  "  processing.mouseReleased = function() {\n" +
+  "    mouseIsPressed = false;\n" +
+  "  };\n" +
+  "  var keyIsPressed = false;\n"+
+  "  processing.keyPressed = function() {\n" +
+  "    keyIsPressed = true;\n" +
+  "  };\n" +
+  "  processing.keyReleased = function() {\n" +
+  "    keyIsPressed = false;\n" +
+  "  };\n" +
+  "  function getSound(s) {\n" +
+  "    var url = 'sounds/' + s + '.mp3';\n" +
+  "    return new Audio(url);\n" +
+  "  }\n" +
+  "  function playSound(s) {\n" +
+  "    s.play();\n" +
+  "  }\n" +
+  "  function getImage(s) {\n" +
+  "    var url = 'https://www.kasandbox.org/programming-images/' + s + '.png';\n" +
+  "    processing.externals.sketch.imageCache.add(url);\n" +
+  "    return processing.loadImage(url);\n" +
+  "  }\n" +
+  "  processing.angleMode = 'degrees';\n" +
+  "  with(processing) {\n";
   PJS_run_env.PJS_end = 
-  "  }" +
-  "  if (typeof draw !== 'undefined') {" +
-  "    processing.draw = draw;" +
-  "  }" +
+  "  }\n" +
+  "  if (typeof draw !== 'undefined') {\n" +
+  "    processing.draw = draw;\n" +
+  "  }\n" +
   "});";
 
   PJS_run_env.run = document.createElement("script");
