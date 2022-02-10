488de01fe0226200551e2bc2ddc18e3d5187ca09
vExcess <41875927+vExcess@users.noreply.github.com>
2022-02-10 14:57:06 -0800

    Update runPJS_onKA.js

--- runPJS_onKA.js
+++ runPJS_onKA.js
@@ -28,6 +28,9 @@ if (pjsEnv.codeElement) {
   "  processingInstance.mousePressed = function() {\n" +
   "    mouseIsPressed = true;\n" +
   "  };\n" +
+  "  processingInstance.mouseClicked = function() {\n" +
+  "    mouseIsPressed = false;\n" +
+  "  };\n" +
   "  processingInstance.mouseReleased = function() {\n" +
   "    mouseIsPressed = false;\n" +
   "  };\n" +
